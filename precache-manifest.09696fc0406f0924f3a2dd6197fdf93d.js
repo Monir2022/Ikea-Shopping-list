aea596a6ecc06779fe7189b32abdb0be4fc55ac5
MonirAkhand <58397306+MonirAkhand@users.noreply.github.com>
2022-01-20 17:12:36 +0100

    Updates

---
+++ precache-manifest.09696fc0406f0924f3a2dd6197fdf93d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb3fa336027667634b482d50c2df3de",
+    "url": "/Ikea-shopping-list/index.html"
+  },
+  {
+    "revision": "ec1f494a4eee0cbf2e3d",
+    "url": "/Ikea-shopping-list/static/css/main.52e93018.chunk.css"
+  },
+  {
+    "revision": "a81a9908717fd81bcb3b",
+    "url": "/Ikea-shopping-list/static/js/2.dfa3fd32.chunk.js"
+  },
+  {
+    "revision": "c5c3fbf8ab7b44579876",
+    "url": "/Ikea-shopping-list/static/js/3.673e4a56.chunk.js"
+  },
+  {
+    "revision": "ec1f494a4eee0cbf2e3d",
+    "url": "/Ikea-shopping-list/static/js/main.9b7652b4.chunk.js"
+  },
+  {
+    "revision": "3d404b5381faa6a220c6",
+    "url": "/Ikea-shopping-list/static/js/runtime~main.dc5cc30f.js"
+  }
+]);
