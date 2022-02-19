031e307eb4ed4514ced536400df23ee2ae0babfa
Monir Akhand <akhandbuet@gmail.com>
2022-02-19 16:18:27 +0100

    change 4

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,19 +2,19 @@ import { List } from "@material-ui/core";
 import React from "react";
 import Todo from "./Todo";
 
-function TodoList({ todos, removeTodo, toggleComplete}) {
+function TodoList({ todos, removeTodo, toggleComplete }) {
   return (
     <div className="ourcenter">
       <List>
-      {todos.map(todo => (
-        <Todo
-          key={todo.id}
-          todo={todo}
-          removeTodo={removeTodo}
-          toggleComplete={toggleComplete}
-        />
-      ))}
-    </List>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            removeTodo={removeTodo}
+            toggleComplete={toggleComplete}
+          />
+        ))}
+      </List>
     </div>
   );
 }
